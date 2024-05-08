71c431cf25bc54d8053b2de836a38674def66eca
Lennart Martens <lennart.in.ny@gmail.com>
2024-05-08 10:33:45 -0400

    Added gitignore and spock

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project is an attempt to automate the fine-tuning process by implementing RLAIF. The goal is to allow individuals and small businesses to create custom models efficiently and easily. These models could have strong domain knowledge, custom output formats, and stabely maintain a role.
 
+To use the package, __init__.py in the methods folder needs to be filled out
+
 Features:
 - LLM builder
 - LLM chat
@@ -8,6 +10,7 @@ Features:
 Roadmap:
 - Data generation for a preference model
 - Fully autonomous RLAIF pipeline
-- Increased speed (bottleneck is API rate limit)
+- Increased speed (bottleneck is API rate limit) (batch predictions?)
+- Split up data generation for reliability and parallelism
 - App/UI for creating custom models
 - Domain knowledge data generation
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from methods.training import *
 
+""" # Example of fine-tuning data generation
 prompts = []
 prompt1 = "The user is swamped with work and feeling anxious. They are looking for support and advice."
 prompt2 = "The user is feeling lonely. They are looking for companionship and reassurance."
@@ -9,6 +10,12 @@ prompts.append((prompt1, 50))
 prompts.append((prompt2, 25))
 prompts.append((prompt3, 70))
 prompts.append((prompt4, 10))
-response_guide = "Respond as though you are Dave, a charming, affectionate, empathetic friend, with an interest in psychology. Never break character. Keep responses brief."
+response_guide = "Respond as though you are Spock, a cold, logical, efficient communicator with little understanding of emotions. Never break character. Keep responses brief."
 
-fine_tuning_data(prompts, response_guide, './dave.jsonl')
+fine_tuning_data(prompts, response_guide, './spock.jsonl')
+"""
+
+""" # Example of chat instance
+model = build_llm("", "projects/82091501274/locations/us-central1/endpoints/8049364098250440704")
+run_chat(model)
+"""
